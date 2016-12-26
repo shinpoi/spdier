facf92f11b8dc3e44c20e9e573783b63d644d08b
aoi-lucario <petersbw123@gmail.com>
2016-12-26 16:33:23 +0900

    ver 0.2, add Regular Expression; add logging, add try-get_guest()

--- osen.py
+++ osen.py
@@ -7,28 +7,37 @@ import time
 import urllib3
 import json
 from bs4 import BeautifulSoup
+import logging
+import re
+
+###################################
+# parameter
+url_id = {'homeraji':'home'}
+save_path = '/home/aoi-lucario/osen/'
+info_file_name = 'info.md'
+log_file_name = 'osen_spider.log'
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(levelname)s]   \t %(asctime)s \t%(message)s\t',
+                    datefmt='%Y/%m/%d (%A) - %H:%M:%S',
+                    filename=log_file_name,
+                    filemode='a')
 
 url_json = 'http://www.onsen.ag/data/api/getMovieInfo/'
 url_homepage = 'http://www.onsen.ag/index.html?pid=home'
 
 ###################################
-# parameter
-url_id = {'homeraji':'home'}
-save_path = '/home/aoi-lucario/tools/'
-info_file_name = 'info.txt'
-
-###################################
 # init
 time_ = time.strftime("%Y/%m/%d (%A) - %H:%M:%S")
 
-# local: 'log.txt', 'flag.json'; path: 'info.txt', 'xxx.mp3'
+# local: log_file_name, 'flag.json'; path: 'info.txt', 'xxx.mp3'
 
 # log (if not, build one)
-if not os.path.exists('./log.txt'):
-    with open('log.txt', 'w') as f:
+if not os.path.exists('./'+log_file_name):
+    with open(log_file_name, 'w') as f:
         f.write('#### log of onsen spider ####\nbuild in %s \n\n' % time_)
 else:
-    with open('log.txt', 'a') as f:
+    with open(log_file_name, 'a') as f:
         f.write('\n#### restart at %s\n\n' % time_)
 
 # info (if not, build one)
@@ -48,7 +57,9 @@ else:
 # spider function
 def get_json(url):
     page_bytes = urllib3.PoolManager().request('GET', url)
-    page_json = json.loads(page_bytes.data[9:-3].decode('utf-8'))
+    pattern = re.compile(r'{.*}')
+    re1 = re.search(pattern, page_bytes.data.decode('utf-8'))
+    page_json = json.loads(re1.group())
     return page_json
 
 
@@ -60,10 +71,10 @@ def get_info(page_json):
     return info
 
 
-def get_guest(homepage):
+def get_guest(homepage, bangumi_id='home'):
     html = urllib3.PoolManager().request('GET', homepage)
     soup = BeautifulSoup(html.data, 'html5lib')
-    info = soup.find(id='home')
+    info = soup.find(id=bangumi_id)
     guest = info['data-guest']
     return guest
 
@@ -74,20 +85,18 @@ def get_mp3(page_json):
     return mp3_file.data
 
 
-def save_files(file, path):
-    with open(path, 'wb') as f:
+def save_files(file, file_name, path=save_path):
+    with open(path+file_name, 'wb') as f:
         f.write(file)
-    with open('log.txt', 'a') as f:
-        f.write('[write]!!!!! write file \'%s\'  at %s \n' % (path, time.strftime("%Y/%m/%d (%A) - %H:%M:%S")))
+    logging.warning('!!!Write files (%s)' % file_name)
 
 
-def add_info(info, info_path):
+def add_info(info, path=save_path, file_name=info_file_name):
     message = "%s 第%s回 -- ゲスト:%s アップデート:%s \n" % (info['title'], info['count'], info['guest'], info['date'])
-    with open(info_path, 'a') as f:
+    with open(path+file_name, 'a') as f:
         f.write(message)
 
-    with open('log.txt', 'a') as f:
-        f.write('[info]!!! write info \'%s\'  at %s \n\n' % (info_path, time.strftime("%Y/%m/%d (%A) - %H:%M:%S")))
+    logging.warning('!!Write info')
 
     # update flag.json
     w = {'homeraji': {'count': info['count']}}
@@ -96,25 +105,28 @@ def add_info(info, info_path):
         f.write(jw)
 
 while True:
-    # print("add log at %s" % time_)
-    with open('log.txt', 'a') as f:
-        f.write('[awake] awake at %s \n' % time.strftime("%Y/%m/%d (%A) - %H:%M:%S"))
+    logging.debug('awake')
 
-    if time.strftime("%H") == '23':
-        # print("add info at %s" % time_)
-        with open('log.txt', 'a') as f:
-            f.write('[scan] scan at %s \n' % time.strftime("%Y/%m/%d (%A) - %H:%M:%S"))
+    if time.strftime("%H") == '16':
+        logging.info('scan')
 
         # spider for homeraji
         uj = url_json + url_id['homeraji']
         page_json = get_json(uj)
         info = get_info(page_json)
         if flag['homeraji']['count'] != info['count']:
-            info['guest'] = get_guest(url_homepage)
-            file_name = 'ほめらじ-' + info['count'] + '.mp3'
+
             mp3 = get_mp3(page_json)
-            save_files(mp3, save_path + file_name)
-            add_info(info, save_path + info_file_name)
+            file_name = 'Home-' + info['count'] + '.mp3'
+            save_files(mp3, file_name=file_name)
+
+            try:
+                info['guest'] = get_guest(url_homepage)
+            except KeyError as e:
+                info['guest'] = 'No Data'
+                logging.warning('No guest data in %s' % file_name)
+            add_info(info)
+
             flag = {'homeraji': {'count': info['count']}}
             print("add new file: %s at %s" % (file_name, time.strftime("%Y/%m/%d (%A) - %H:%M:%S")))
     time.sleep(60*60)
