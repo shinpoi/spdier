3a46693ecfce8a4bdcbf91d9723b7abf23350fb2
aoi-lucario <petersbw123@gmail.com>
2021-04-22 17:22:58 +0900

    Create echo_server.py

---
+++ echo_server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route("/")
+@app.route("/<path:path>")
+def echo_server(path):
+    url = request.full_path
+    method = request.method
+    url_args = request.args
+    header = request.headers
+    data = request.data.decode()
+
+
+    return f'''======
+URL: {method} {url}
+--
+header:
+{header}
+--
+data:
+{data}
+'''
+
+if __name__ == "__main__":
+    app.run('127.0.0.1', 8080)
+    # app.run('0.0.0.0', 8080)
